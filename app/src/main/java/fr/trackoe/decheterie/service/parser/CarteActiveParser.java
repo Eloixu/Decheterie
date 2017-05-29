d7a01642baa27253bc8155554493a73e731d01f4
Remi C <remi.coquet@gmail.com>
2017-05-29 19:15:23 +0200

    fix carte_etat_raison_id isnull

--- app/src/main/java/fr/trackoe/decheterie/service/parser/CarteActiveParser.java
+++ app/src/main/java/fr/trackoe/decheterie/service/parser/CarteActiveParser.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import fr.trackoe.decheterie.Utils;
 import fr.trackoe.decheterie.model.bean.global.CarteActives;
 
 /**
@@ -29,7 +30,7 @@ public class CarteActiveParser extends JSONParser<CarteActives> {
 
                     int     carteId             = jobj.has("carte_id")                   ? jobj.getInt("carte_id") : -1;
                     boolean isActive            = jobj.has("is_active")                  ? jobj.getBoolean("is_active") : false;
-                    int     carteEtatRaisonId   = jobj.has("carte_etat_raison_id")       ? jobj.getInt("carte_etat_raison_id") : -1;
+                    int     carteEtatRaisonId   = jobj.has("carte_etat_raison_id") && !Utils.isStringEmpty(jobj.getString("carte_etat_raison_id"))       ? jobj.getInt("carte_etat_raison_id") : -1;
                     String  dateActivation      = jobj.has("date_activation")            ? jobj.getString("date_activation") : "";
                     String  dateDM              = jobj.has("date_dernier_modif")         ? jobj.getString("date_dernier_modif") : "";
                     int     comptePrepayeId     = jobj.has("compte_prepaye_id")          ? jobj.getInt("compte_prepaye_id") : -1;
